e7809eb38df1799217ded2e61adc2ae4423c09ef
Andrei Dzebysh <presnia@yahoo.com>
2020-10-14 00:21:48 +0300

    Finished part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      
-      
-    </div>
+    <>
+    <Header />
+    <Main />
+    
+    </>
   );
 }
 
