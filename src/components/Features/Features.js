5606a3e8cefe54f51852033ed6ff360ac515bc35
Presnia <presnia@yahoo.com>
2021-02-04 14:08:30 +0300

    The Features element has been modified.

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -10,17 +10,20 @@ const rocketImages = {
 	'Starship': 'starship',
 };
 
-const Features = ({
-	name, 
-	height, 
-	diameter, 
-	mass, 
-	payload_weights: payloadWeights, 
-	description
-  }) => (
-		<>
+const Features = (props) => {
+	const {
+		  name,
+			height,
+			diameter,
+			mass,
+			payload_weights: payloadWeights,
+			description
+	} = props;
 
-		<Main rocket={name} />
+	  return (
+			<>
+
+				<Main rocket={name} />
 				<section className="features">
 					<h2 className="features-title">
 						{name} <br/>Overview
@@ -32,26 +35,26 @@ const Features = ({
 								Size
 							</caption>
 							<thead>
-								<tr>
-									<td className="table-column">HEIGHT</td>
-									<td className="table-column">{height.meters} m / {height.feet} ft</td>
-								</tr>
-								<tr>
-									<td className="table-column">DIAMETER</td>
-									<td className="table-column">{diameter.meters} m / {diameter.feet} ft</td>
-								</tr>
-								<tr>
-									<td className="table-column">MASS</td>
-									<td className="table-column">{mass.kg} kg / {mass.lb} lb</td>
-								</tr>
+							<tr>
+								<td className="table-column">HEIGHT</td>
+								<td className="table-column">{height.meters} m / {height.feet} ft</td>
+							</tr>
+							<tr>
+								<td className="table-column">DIAMETER</td>
+								<td className="table-column">{diameter.meters} m / {diameter.feet} ft</td>
+							</tr>
+							<tr>
+								<td className="table-column">MASS</td>
+								<td className="table-column">{mass.kg} kg / {mass.lb} lb</td>
+							</tr>
 
-								{payloadWeights.map((item) => (
-										<tr key={item.id}>
-											<td className="table-column">PAYLOAD TO {item.id.toUpperCase()}</td>
-											<td className="table-column">{item.kg} kg / {item.lb} lb</td>
-										</tr>
-								))}
-								
+							{payloadWeights.map((item) => (
+								<tr key={item.id}>
+									<td className="table-column">PAYLOAD TO {item.id.toUpperCase()}</td>
+									<td className="table-column">{item.kg} kg / {item.lb} lb</td>
+								</tr>
+							))}
+
 							</thead>
 						</table>
 						<RelaxWrapper speed={14}>
@@ -61,7 +64,7 @@ const Features = ({
 								className="rocket"
 							/>
 						</RelaxWrapper>
-						
+
 						<article>
 							<h3 className="features-subtitle">DESCRIPTION</h3>
 							<p className="features-text">
@@ -70,7 +73,10 @@ const Features = ({
 						</article>
 					</div>
 				</section>
-				</>
-		);
+
+			</>
+		)
+	};
+
 
 export default Features;
